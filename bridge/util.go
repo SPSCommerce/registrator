beb711f1c57ebc977a6f6acdb6e7fe4781735ab1
jmhoeft <jmhoeft@spscommerce.com>
2021-04-26 15:46:54 -0500

    go fmt

--- bridge/util.go
+++ bridge/util.go
@@ -83,7 +83,7 @@ func serviceMetaData(config *dockerapi.Config, port string) (map[string]string, 
 		if (kvp[0] == "com.amazonaws.ecs.task-arn") && len(kvp) > 1 {
 			key := "taskId"
 			value := strings.SplitN(value, "/", -1)[2]
-	        metadata[key] = value
+			metadata[key] = value
 		}
 	}
 	return metadata, metadataFromPort
